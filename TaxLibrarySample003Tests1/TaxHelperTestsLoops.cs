96f65450408584c7f615eb9dec096b42dddbf29b
陸普世 <Beethovenlu@hotmail.com>
2017-08-04 08:16:43 +0800

    * TestLoops   - GetTaxResultTest_Income_100000_Tax_5000     > Pass

---
+++ TaxLibrarySample003Tests1/TaxHelperTestsLoops.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TexLibrarySample003;
+
+namespace TaxLibrarySample003Tests1
+{
+    [TestClass]
+    public class TaxHelperTestsLoops
+    {
+        [TestMethod()]
+        public void GetTaxResultTest_Income_100000_Tax_5000()
+        {
+            decimal income = 100000;
+            decimal actual = TaxHelper.GetTaxResult(income);
+            decimal expected = 5000;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ TexLibrarySample003/TaxHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexLibrarySample003
+{
+    public class TaxHelper
+    {
+        public static decimal GetTaxResult(decimal income)
+        {
+            var r0 = 0m;
+            var r1 = 0m;
+            var f0 = 0m;
+            var f1 = 0.05m;
+
+            var result = (r1 - r0) * f0 + (income - r1) * f1;
+
+            return result;
+        }
+    }
+}
