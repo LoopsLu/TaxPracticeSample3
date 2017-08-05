3b0e944f6a11c825d846d57f9e45ed4ca7078b49
陸普世 <Beethovenlu@hotmail.com>
2017-08-05 11:06:57 +0800

    * TaxHelper   - _incomeLevel     > Renamed from _r   - _taxRatio     > Renamed from _f

--- TexLibrarySample003/TaxHelper.cs
+++ TexLibrarySample003/TaxHelper.cs
@@ -8,25 +8,25 @@ namespace TexLibrarySample003
 {
     public class TaxHelper
     {
-        private static List<decimal> _r = new List<decimal>()
+        private static List<decimal> _incomeLevel = new List<decimal>()
         { 0m, 0m, 540000m, 1210000m, 2420000m, 4530000m, 10310000m, decimal.MaxValue};
 
-        private static List<decimal> _f = new List<decimal>()
+        private static List<decimal> _taxRatio = new List<decimal>()
         { 0m, 0.05m, 0.12m, 0.2m, 0.3m, 0.4m, 0.5m};
 
         public static decimal GetTaxResult(decimal income)
         {
             var result = 0m;
 
-            for (int i = 1; i < _f.Count; i++)
+            for (int i = 1; i < _taxRatio.Count; i++)
             {
-                if (income > _r[i])
-                    result += (_r[i] - _r[i - 1]) * _f[i - 1];
+                if (income > _incomeLevel[i])
+                    result += (_incomeLevel[i] - _incomeLevel[i - 1]) * _taxRatio[i - 1];
                 else
                     break;
 
-                if (income > _r[i] && income <= _r[i + 1])
-                    result += (income - _r[i]) * _f[i];
+                if (income > _incomeLevel[i] && income <= _incomeLevel[i + 1])
+                    result += (income - _incomeLevel[i]) * _taxRatio[i];
             }
 
             return result;
