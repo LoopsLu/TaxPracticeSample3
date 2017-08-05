d0a78677c8dba22b7c5ed14c3d5be543c310d903
陸普世 <Beethovenlu@hotmail.com>
2017-08-05 10:17:40 +0800

    * TaxHelper   - GetTaxResult()     > Use "i" to simplify and unite the repeat codes

--- TexLibrarySample003/TaxHelper.cs
+++ TexLibrarySample003/TaxHelper.cs
@@ -17,24 +17,27 @@ namespace TexLibrarySample003
         public static decimal GetTaxResult(decimal income)
         {
             var result = 0m;
-            
-            if (income > _r[1])
-                result += (_r[1] - _r[0]) * _f[0];
 
-            if (income > _r[1] && income <= _r[2])
-                result += (income - _r[1]) * _f[1];
+            var i = 1;
+            if (income > _r[i])
+                result += (_r[i] - _r[i-1]) * _f[i-1];
 
-            if (income > _r[2])
-                result += (_r[2] - _r[1]) * _f[1];
+            if (income > _r[i] && income <= _r[i+1])
+                result += (income - _r[i]) * _f[i];
 
-            if (income > _r[2] && income <= _r[3])
-                result += (income - _r[2]) * _f[2];
+            i = 2;
+            if (income > _r[i])
+                result += (_r[i] - _r[i-1]) * _f[i-1];
 
-            if (income > _r[3])
-                result += (_r[3] - _r[2]) * _f[2];
+            if (income > _r[i] && income <= _r[i+1])
+                result += (income - _r[i]) * _f[i];
 
-            if (income > _r[3] && income <= _r[4])
-                result += (income - _r[3]) * _f[3];
+            i = 3;
+            if (income > _r[i])
+                result += (_r[i] - _r[i - 1]) * _f[i - 1];
+
+            if (income > _r[i] && income <= _r[i + 1])
+                result += (income - _r[i]) * _f[i];
 
             return result;
         }
